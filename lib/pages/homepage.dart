b1eb6cec06cedb6ee3fc373ede9f9227090c13c5
TsalemisIasonas <tsalemisi@gmail.com>
2023-12-29 17:21:57 +0200

    update, categorytrigger

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -10,33 +10,47 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  void category1() {}
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  void categorytrigger() {
+    maincolor = Colors.yellow;
+    setState(() {
+      
+    });
+    // Add your logic for handling the category press here
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
-        appBar: AppBar(
-          backgroundColor: backgroundColor,
-          elevation: 0.5,
-          shadowColor: Colors.red,
-          centerTitle: true,
-          title: const Text("Music Practice App"),
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        elevation: 0.5,
+        shadowColor: Colors.red,
+        centerTitle: true,
+        title: const Text("Music Practice App"),
+      ),
+      body: Center(
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: [
+            Padding(
+              padding: EdgeInsets.fromLTRB(50.0, 30.0, 50.0, 30.0),
+              child: Category("Category 1", Colors.deepOrangeAccent, categorytrigger),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(50.0, 30.0, 50.0, 30.0),
+              child: Category("Category 2", Colors.green, categorytrigger),
+            ),
+          ],
         ),
-        body: Center(
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: const [
-              Padding(
-                padding: EdgeInsets.fromLTRB(50.0, 30.0, 50.0, 30.0),
-                child: Category("Category 1", Colors.deepOrangeAccent),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20.0),
-                child: Category("Category 2", Colors.green),
-              )
-            ], // Pass the category string to the Category widget
-          ),
-        ));
+      ),
+    );
   }
 }
--- lib/util/category.dart
+++ lib/util/category.dart
@@ -2,12 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:music_app/constants/colors.dart';
 
 class Category extends StatelessWidget {
-  final String category; // Declare a field to hold the category string
+  final String category;
   final Color choiceColor;
+  final VoidCallback categorytrigger;
 
-  const Category(this.category, this.choiceColor, {Key? key}) : super(key: key);
-
-  void categorytrigger() {}
+  const Category(this.category, this.choiceColor, this.categorytrigger, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,14 +15,13 @@ class Category extends StatelessWidget {
       child: TextButton(
         onPressed: categorytrigger,
         style: TextButton.styleFrom(
-            foregroundColor: choiceColor,
-            backgroundColor: maincolor,
-            shadowColor: choiceColor,
-            minimumSize:
-                Size((3 * screenSize.width) / 4, 3 * screenSize.height / 4),
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-            side: BorderSide(color: choiceColor, width: 2.0)),
+          foregroundColor: choiceColor,
+          backgroundColor: maincolor,
+          shadowColor: choiceColor,
+          minimumSize: Size((3 * screenSize.width) / 4, 3 * screenSize.height / 4),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          side: BorderSide(color: choiceColor, width: 2.0),
+        ),
         child: Text(
           category,
         ),
